1f2f0cca67f36d4b4b39f1527acecba2c3d6e16f
driverInside <hiram.eps@gmail.com>
2020-05-23 22:27:43 -0500

    checkpoint

---
+++ app/js/app.js
@@ -0,0 +1,91 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    answers: [], 
+    questions: [
+      {
+          "id": 1,
+          "question": "What does CSS stand for?",
+          "nextId": 2,
+          "previousId": null,
+          "answer": "c",
+          "options": {
+              "a": "Computer Style Sheets",
+              "b": "Creative Style Sheets",
+              "c": "Cascading Style Sheets",
+              "d": "Colourful Style Sheets"
+          }
+      },
+      {
+          "id": 2,
+          "question": "Where in an HTML document is the correct place to refer to an external style sheet?",
+          "nextId": 2,
+          "previousId": 1,
+          "answer": "a",
+          "options": {
+              "a": "In the <head> section",
+              "b": "In the <body> section",
+              "c": "At the end of the document",
+              "d": "You can't refer to an external style sheet"
+          }
+      },
+      {
+          "id": 3,
+          "question": "Which HTML tag is used to define an internal style sheet?",
+          "nextId": 4,
+          "previousId": 2,
+          "answer": "a",
+          "options": {
+              "a": "<style>",
+              "b": "<script>",
+              "c": "<headStyle>",
+              "d": "<css>"
+          }
+      },
+      {
+          "id": 4,
+          "question": "Which HTML attribute is used to define inline styles?",
+          "nextId": null,
+          "previousId": 3,
+          "answer": "b",
+          "options": {
+              "a": "class",
+              "b": "style",
+              "c": "font",
+              "d": "styles"
+          }
+      }
+    ]
+  },
+  methods: {
+    selectOption: function (evt) {
+      if (evt) {
+        
+
+        const element = evt.target
+        const answer = element.dataset.key
+        
+        const index =  parseInt(element.dataset.index) + 1
+        const question = this.getQuestion(index)
+        if (answer === question.answer) {
+          console.log('-----------')
+          console.log('Correcta')
+          console.log('-----------')
+        } else {
+          console.log('-----------')
+          console.log('incorrecta')
+          console.log('-----------')
+        }
+      }
+    },
+    getQuestion: function (index) {
+      for (let i = 0; i < this.questions.length; i++) {
+        if (index === this.questions[i].id) {
+          return this.questions[i]
+        }
+      }
+      return false
+    }
+  }
+})
